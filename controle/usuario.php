3b985dd2aecfc1fbcdbfc6ccdc10e13653e64b7a
Ilio Junior <junior@ingadigital.com.br>
2017-02-15 09:52:00 -0200

    Implementacoes

--- controle/usuario.php
+++ controle/usuario.php
@@ -18,8 +18,16 @@ for($i = 0; $i < strlen(UserUtil::WORD_KEY); $i++){
             <h3><?= $novoUsuario ? "Novo Usu&aacute;rio" : $usuario['nome'] ?></h3>
         </div>
         <form action="?file=save.php&action=save" method="post" id="formulario-user">
+
+                <input type="checkbox" id="is_ativo-field" name="is_ativo-field" checked="<?= ($novoUsuario ? "unchecked" : ($usuario['is_ativo'] === 'Y' ? "checked" : "unchecked" )) ?>" />
+                <label for="is_ativo-field">Ativo</label>
+
+
             <input type="hidden" name="key" value="<?= $key ?>" />
             <input type="hidden" name="id_user" value="<?= $id ?>" />
+
+            <div class="row"></div>
+
             <div class="row">
                 <div class="input-field col s12 m6 l6">
                     <input id="nome" name="nome" type="text" class="validate"
@@ -32,6 +40,7 @@ for($i = 0; $i < strlen(UserUtil::WORD_KEY); $i++){
                     <label for="email">Email:</label>
                 </div>
             </div>
+
             <div class="row">
                 <div class="input-field col s12 m6 l6">
                     <input id="login" name="login" type="text" class="validate"
@@ -40,29 +49,31 @@ for($i = 0; $i < strlen(UserUtil::WORD_KEY); $i++){
                 </div>
                 <div class="input-field col s12 m6 l6">
                     <input id="senha" name="senha" type="password" class="validate"
-                           value="<?= $novoUsuario ? "" : $usuario['senha'] ?>"/>
+                           value=""/>
                     <label for="login">Senha:</label>
                 </div>
             </div>
+
             <div class="row" id="nova-senha">
                 <div class="input-field col s12 m6 l6">
-                    <input id="login" name="login" type="text" class="validate"
-                           value="<?= $novoUsuario ? "" : $usuario['login'] ?>"/>
-                    <label for="login">Nova senha:</label>
+                    <input id="nova-senha-field" name="nova-senha" type="text" class="validate"
+                           value=""/>
+                    <label for="nova-senha-field">Nova senha:</label>
                 </div>
                 <div class="input-field col s12 m6 l6">
-                    <input id="senha" name="senha" type="password" class="validate"
-                           value="<?= $novoUsuario ? "" : $usuario['senha'] ?>"/>
-                    <label for="login">Repita a senha:</label>
+                    <input id="confirm-nova-senha" name="confirm-nova-senha" type="password" class="validate"
+                           value=""/>
+                    <label for="confirm-nova-senha">Repita a senha:</label>
                 </div>
             </div>
+
             <div class="row">
-                <div class="input-field col s6 m6 l12">
-                    <input id="nome" name="nome" type="text" class="validate"
-                           value="<?= $novoUsuario ? "" : $usuario['nome'] ?>"/>
-                    <label for="nome">Nome Completo:</label>
+                <div class="input-field col s12 m6 l6">
+
+                    <label>Nome Completo:</label>
                 </div>
             </div>
+
         </form>
     </div>
     <script>
